36288b912283c437e194a73bbee43a1e9aedaa52
Marcos Maceo <marcosmaceowork@gmail.com>
2019-11-28 11:47:36 -0500

    Add Method to convert an Interface to []string

---
+++ src/util/util.go
@@ -0,0 +1,22 @@
+package util
+
+import (
+	"errors"
+	"reflect"
+	"strconv"
+)
+
+func StrArrayFromInterface(i interface{}) ([]string, error) {
+	object := reflect.ValueOf(i)
+	items := make([]string, 0)
+
+	for i := 0; i < object.Len(); i++ {
+		val, ok := object.Index(i).Interface().(string)
+		if !ok {
+			return items, errors.New("error getting item at index " + strconv.FormatInt(int64(i), 10))
+		}
+		items = append(items, val)
+	}
+
+	return items, nil
+}
